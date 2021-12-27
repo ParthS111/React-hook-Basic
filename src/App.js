a4085cb03693e230a2ca0e529d8b854f2cbb723f
parth <parths@itpathsolutions.co.in>
2021-12-27 13:59:06 +0530

    new work

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import './App.css';
-import UseContext from './Hooks/useContext';
-import UseEffect from './Hooks/useEffect';
-import UseState from './Hooks/useState';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Error from './Pages/Error';
+import React from 'react'
+
 function App() {
-  return (
-   <Router>
-     <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/about' element={<About/>}/>
-       <Route path='/contact/' element={<Contact/>}/>
-       <Route path='/contact/:userName' element={<Contact/>}/>
-       <Route path='*' element={<Error/>}/>
-     </Routes>
-   </Router>
-  );
+    return (
+        <div>
+            <h1>Parth</h1>
+        </div>
+    )
 }
 
-export default App;
- // <div className="App">
-    //    {/* <UseState /> */}
-    //    {/* <UseEffect /> */}
-    //    <UseContext />
-    // </div>
+export default App
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import StateProvider from './Hooks/stateProvider';
+import StateProvider from './Basic/Hooks/stateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
